e928f4dd155c2cfddd544f7d673505e182bf1bce
Juan Felipe Barahona Gonzalez <jbarahona@XM.com.co>
2022-10-24 15:34:51 -0500

    return a token for a new user was implemented

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -1,17 +1,20 @@
 import { Request, Response } from "express"
+import jwt from 'jsonwebtoken'
+
 import User, { IUser } from "../models/user"
 
 export const signup = async (req: Request, res: Response) => {
-
+  // saving a new user
   const user: IUser = new User({
     username: req.body.username,
     password: req.body.password,
     email: req.body.email,
   })
   const savedUser = await user.save()
-  console.log(savedUser);
+  // tokenizing
+  const token: string = jwt.sign({_id: savedUser._id}, process.env.TOKEN_SECRET || 'tokenfake')
   
-  res.send("Signup")
+  res.header('auth-token', token).json(savedUser)
 }
 
 export const signin = (req: Request, res: Response) => {
